149f632533848ffdf89a120edeec5b4060d756ff
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-16 06:58:03 -0700

    duben

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main function of the program
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
