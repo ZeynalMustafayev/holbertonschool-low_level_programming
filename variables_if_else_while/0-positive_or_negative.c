d5a319d9d46c0cb77c01e4de16cffb82214852f9
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-16 05:22:27 -0700

    duben

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+}
