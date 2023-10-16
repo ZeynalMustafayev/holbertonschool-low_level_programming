3b606bd44d25efafdb78797b87f9b25843ba3a93
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-16 07:40:46 -0700

    duben

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this program prints alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 0 ; a < 10; a++)
+	{
+		if (a % 2 != 0)
+		{
+			putchar(a);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
