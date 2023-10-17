87a05949334e0a1abaf45ae5a7eac40f60a960a4
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-17 05:30:10 -0700

    duben

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,9 +9,14 @@ int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 16; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		putchar(a++);
+		putchar(a);
+	}
+
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
 	}
 	putchar ('\n');
 	return (0);
