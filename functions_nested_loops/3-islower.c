0ebbadf75bc7039dc34852ab58e6260c2d9c16b8
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-18 02:36:32 -0700

    duben

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints every character of alphabet
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
