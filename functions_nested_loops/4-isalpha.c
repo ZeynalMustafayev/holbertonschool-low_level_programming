b535164b4e4b0eab447476848924a8b23ebf567a
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-18 02:50:54 -0700

    duben

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _isalpha - check lowercase or uppercase.
+ *
+ * @c: variable
+ *
+ * Return: if islower or uppercase 1, otherwise 0.
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
