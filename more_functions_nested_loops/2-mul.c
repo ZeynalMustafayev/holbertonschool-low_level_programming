7780b84413b80e80d44a92b97be9ed0e1ac0afb2
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-19 08:04:15 -0700

    duben

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - print multiple two integers.
+ *
+ * @a: variable1
+ *
+ * @b: variable2
+ *
+ * Return: result of multiple
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
