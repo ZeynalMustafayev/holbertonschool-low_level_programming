c19235efa147ddd162d5f8d422a31e7f3c00dd40
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-20 07:07:59 -0700

    duben

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a = 1;
+
+	for (; a <= 100; a++)
+	{
+		if (a % 5 == 0 && a % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz")
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		putchar (' ');
+	}
+	putchar ('\n');
+	return (0);
+}
