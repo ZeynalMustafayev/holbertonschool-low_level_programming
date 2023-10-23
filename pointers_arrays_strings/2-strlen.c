2ecaf0711389455e54a356e549fc0e978c50fb80
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-23 07:01:01 -0700

    duben

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - length of string
+ * @s: variable
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	a = strlen(s);
+	return (a);
+}
