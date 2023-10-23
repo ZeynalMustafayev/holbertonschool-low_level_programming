7b4df2a92d94d99892758aaece3478f2faa7381f
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-23 06:24:38 -0700

    duben

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - check the code
+ *
+ * @a:pointer
+ *
+ * @b:pointer
+ *
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
