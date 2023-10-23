4d1d7cb997e4d7d75c00e50080fafd20995ddc2b
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-23 07:21:12 -0700

    duben

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - length of string
+ * @str: variable
+ * Return: integer
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
