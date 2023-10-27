c6b411aa3c85eb7094c12f0f9485a2766ec38503
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-27 05:37:12 -0700

    duben

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - Something useful
+ * dest: Something more useful
+ * src: Something more useful
+ * n - Something more useful
+ * @s: Something more useful
+ * @c: Something more useful
+ *
+ * Return: Something much more useful
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
