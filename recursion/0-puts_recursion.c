ea5e4b012f49866cb2a915f217a8094c142a89c7
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-30 05:18:24 -0700

    duben

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - length of string
+ * @s : variable
+ * Return: integer
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
---
+++ recursion/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+void _puts_recursion(char *s);
+#endif
