aa438e1d1af9f75bf427c286a0544a49c9d4aa36
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-10-30 07:23:06 -0700

    ff

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - length of string
+ * @x : variable
+ * @y : variable
+ * Return: integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x , y - 1));
+}
