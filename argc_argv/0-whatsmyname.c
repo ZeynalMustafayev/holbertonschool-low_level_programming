1e30d909a1ab66787b5bb6039a75e6181f2aea0c
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-01 05:20:46 -0700

    duben

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * @argc : variable
+ *
+ * @argv : variable
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
