d800a72ed9027d5cc408080d52bb06509dce1913
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-01 07:39:28 -0700

    duben

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * @argc : variable
+ *
+ * @argv : variable
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int result = 1;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		result = result * strtol(argv[i], NULL, 10);
+	}
+	printf("%d\n", result);
+	return (0);
+}
