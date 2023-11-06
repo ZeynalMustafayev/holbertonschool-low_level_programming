e44e94db308cb8d5fe8d2229fcd96898800d7b83
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-06 09:29:04 -0800

    It's not bragging if you can back it up

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * free_grid - prints buffer in hexa
+ * @grid: the address of memory to print
+ * @height: the address of memory to print
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
