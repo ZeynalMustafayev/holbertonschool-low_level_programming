e22f27a856562066ad74e1105c77a8b6c8b8719d
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-06 06:55:41 -0800

    He who is not courageous enough to take risks will accomplish nothing in life

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * str_concat - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int a, b;
+	char *ar;
+	int i;
+
+	if (s1 == NULL)
+		return (NULL);
+
+	if (s2 == NULL)
+		return (NULL);
+
+	a = strlen(s1);
+	b = strlen(s2);
+
+	ar = malloc(sizeof(char) * (a + b) + 1);
+
+	for (i = 0; i < a; i++)
+		ar[i] = s1[i];
+
+	for (i = 0; i < b; i++)
+		ar[i + a] = s2[i];
+
+	ar[i + a] = '\0';
+	return (ar);
+}
