47cb30631d32799122657962796790cab1fceab7
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-06 05:36:44 -0800

    duben

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ * _strdup - prints buffer in hexa
+ * @str: the address of memory to print
+ *
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	char *ar;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i])
+		i++;
+
+	ar = malloc(sizeof(str) * i + 1);
+
+	while (str[j])
+	{
+		ar[j] = str[j];
+		j++;
+	}
+	return (ar);
+}
