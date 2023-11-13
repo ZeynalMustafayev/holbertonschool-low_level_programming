15b0f08f005af12fd757af486ab621a6f024adf7
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-13 03:33:46 -0800

    duben

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -11,13 +11,13 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int a1, a3;
+	unsigned int a1, a2, a3;
 	unsigned int i = 0;
 	char *a;
 
 	a1 = strlen(s1);
 	n = strlen(s2);
-	a3 = a1 + n + 1;
+	a3 = a1 + a2 + 1;
 
 	a = malloc(sizeof(char) * a3);
 
@@ -27,8 +27,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i <= a1; i++)
 	a[i] = s1[i];
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= a2; i++)
 	a[i + a1] = s2[i];
+
+	if (n > a2)
+		return (a2);
 
 	a[i] = '\0';
 
