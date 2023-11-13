633fc74350881f082414e26978e955e31eae7ad8
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-13 05:15:33 -0800

    string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -37,12 +37,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (a == NULL)
 		return (NULL);
 
-	for (i = 0; i < a1; i++)
+	for (i = 0; i < a1; i + 1)
 	{
 		a[i] = s1[i];
 	}
 
-	for (j = 0; j < sign; j++)
+	for (j = 0; j < sign; j + 1)
 	{
 		a[i] = s2[j];
 		i++;
