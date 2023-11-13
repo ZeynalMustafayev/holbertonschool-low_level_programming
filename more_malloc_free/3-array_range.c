2c4130b7b3ff440a33365d8b973259569a7c995c
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-13 07:43:51 -0800

    3. array_range

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0; i < max - min + 1; i++)
-		ar[i] = i;
+	for (min = 0; i <= max; i++)
+		ar[i - min] = i;
 	return (ar);
 }
