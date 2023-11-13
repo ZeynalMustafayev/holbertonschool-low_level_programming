aef25a9d0e120b993818576efaedd52e4a4aeabc
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-13 03:30:32 -0800

    duben

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,27 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * string_nconcat - prints buffer in hexa
+ * @s1: the address of memory to print
+ * @s2: the address of memory to print
+ * @n: the address of memory to print
+ * Return: Nothing.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int a1, a3;
-        unsigned int i = 0;
+	unsigned int i = 0;
 	char *a;
 
 	a1 = strlen(s1);
 	n = strlen(s2);
 	a3 = a1 + n + 1;
 
-	a =malloc(sizeof(char) * a3);
+	a = malloc(sizeof(char) * a3);
 
-	for(i = 0; i <= a1; i++)
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i <= a1; i++)
 	a[i] = s1[i];
 
-	for(i = 0; i <= n; i++)
-	a[i+a1] = s2[i];
+	for (i = 0; i <= n; i++)
+	a[i + a1] = s2[i];
 
-	a[i]='\0';
+	a[i] = '\0';
 
-	return a;
-    
+	return (a);
 }
