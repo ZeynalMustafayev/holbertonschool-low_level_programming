9358df94affc03e57e1fe02dd478870865eedae3
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-13 07:36:21 -0800

    3. array_range

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * array_range - prints buffer in hexa
+ * @max: the address of memory to print
+ * @min: the address of memory to print
+ * Return: Nothing.
+ */
+int *array_range(int min, int max)
+{
+	int *ar;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	ar = malloc(sizeof(int) * (max - min + 1));
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < max - min + 1; i++)
+		ar[i] = i;
+	return (ar);
+}
