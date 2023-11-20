9a05932ce448065940a1dc180e9bdfe03fd290d8
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-20 05:32:23 -0800

    1. A dog is the only thing on earth that loves you more than you love yourself

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - Short description
+ * @d: First member
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
