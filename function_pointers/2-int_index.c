3341a5c485851657ed54659bb727c859179bf76c
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-21 06:18:35 -0800

    2. To hell with circumstances; I create opportunities

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+/**
+ * int_index - print a name in uppercase
+ * @array: name of the person
+ * @size: name of the person
+ * @cmp: name of the person
+ *
+ * Return: Nothing.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (0);
+}
