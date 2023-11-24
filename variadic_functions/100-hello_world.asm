047344e7da1b34b1f255c498f834c2ce09d93eb5
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-11-24 06:18:39 -0800

    4. Real programmers can write assembly code in any language

---
+++ variadic_functions/100-hello_world.asm
@@ -0,0 +1,16 @@
+segment .data:
+	message db "Hello, World", 0xA
+	message_length equ $ - message
+
+segment .text:
+	global main
+main:
+  	mov eax, 0x4
+	mov ebx, 1
+	mov ecx, message
+	mov edx, message_length
+	int 0x80
+	
+	mov eax, 0x1
+	mov ebx, 0
+	int 0x80
