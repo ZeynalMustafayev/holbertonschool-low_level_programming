bbd1577843e1760541731d2c37fda2fb09e83d94
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-04 05:47:20 -0800

    2. Add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,9 +16,9 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	int c;
 
-	for (c = 0; str[c] != '\0'; c++);
+	for (c = 0; str[c] != '\0'; c++)
+		;
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 	{
 		free(new_node);
