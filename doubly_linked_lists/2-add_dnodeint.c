f654dbddda9c68a0e9151676ce04d2ea58394919
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-05 05:24:26 -0800

    2. Add node

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_dnodeint - prints each element of linked list with lengths.
+ *
+ * @head: first node of linked list.
+ *
+ * @n: the text of new node.
+ *
+ * Return: the number of nodes in given linked list.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *ar;
+
+	ar = malloc(sizeof(size_t));
+	ar->n = n;
+	ar->prev = NULL;
+	ar->next = *head;
+
+	if ((*head) != NULL)
+	{
+		(*head)->prev = ar;
+	}
+	(*head) = ar;
+	return (ar);
+}
