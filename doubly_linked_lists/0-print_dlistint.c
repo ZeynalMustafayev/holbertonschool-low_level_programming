3aa3c5d88a2a461a791051fd6bc1af4d073a7793
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-05 04:56:50 -0800

    0. Print list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -2,10 +2,16 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_dlistint - prints each element of linked list with lengths.
+ *
+ * @h: struct that provides string, length and address of next node.
+ *
+ * Return: the number of nodes in given linked list.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t size;
+	size_t size = 0;
 
 	while (h != NULL)
 	{
