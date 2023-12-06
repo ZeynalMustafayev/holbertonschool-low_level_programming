d59b1b5b501f60d1afae6b14bbe13fefba52f536
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-06 03:47:07 -0800

    6. Sum list

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -3,20 +3,21 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * get_dnodeint_at_index - function that takes particular node given index.
+ * sum_dlistint - function that takes particular node given index.
  *
  * @head: given linked list.
  *
  * Return: particular node situated in given index.
  */
-int sum_dlistint(dlistint_t *head) 
-{ 
-	dlistint_t *ptr = head; 
-	int sum = 0; 
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *ptr = head;
+	int sum = 0;
+
 	while (ptr != NULL)
 	{
-		sum += ptr->n; 
-		ptr = ptr->next; 
+		sum += ptr->n;
+		ptr = ptr->next;
 	}
-	return (sum); 
-} 
+	return (sum);
+}
