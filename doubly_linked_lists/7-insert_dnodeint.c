683f0602a6945a98e45f262ca66be4965bc8730b
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-06 06:09:51 -0800

    7. Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,17 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * insert_dnodeint_at_index - function that takes particular node given index.
+ *
+ * @h: given linked list.
+ *
+ * @idx: index
+ *
+ * @n: count
+ *
+ * Return: particular node situated in given index.
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *prev_node = *h;
@@ -11,11 +21,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	
 	if (new_node)
 	{
 		new_node->n = n;
-		if (!idx)
+		if (idx == 0)
 		{
 			if (*h)
 			{
@@ -38,6 +47,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->next = prev_node->next;
 			new_node->prev = prev_node;
 			prev_node->next = new_node;
+			return (new_node);
 		}
 	}
 	return (NULL);
