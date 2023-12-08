326c144611c712d6ba32c8775babae8f9adfcb6a
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-08 03:30:35 -0800

    8. Delete at index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,9 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *node;
-	unsigned int i = 0;	
+
+	unsigned int i = 0;
+
 	if (!*head)
 		return (-1);
 	node = (*head);
