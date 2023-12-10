e289327def828fcdecb9d789ac39885fac6e5c40
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-10 08:48:18 -0800

    2.10

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ * get_bit - get value of bit at given index.
+ *
+ * @n: given integer.
+ * 
+ * @index: which index we need.
+ * 
+ * Return: a bit which we want to find.
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int a = sizeof(unsigned long int) * 8;
+
+	if (index >= a)
+		return (-1);
+	n = n >> index & 1;
+	return (n);
+}
