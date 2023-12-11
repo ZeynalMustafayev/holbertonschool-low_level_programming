1230a31eacc9c1dcff1077eced4bd32f6fbb324d
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-11 08:12:47 -0800

    Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,7 +6,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int i = 0, file;
 
-	file = open(filename,  O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename,  O_TRUNC | O_WRONLY | O_CREAT, 0600);
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
