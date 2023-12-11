615618377e4fb3e4dfa487f0068ddba4544517c4
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-11 08:48:29 -0800

    Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+/**
+ * append_text_to_file - that appends text at the end of a file
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: function that appends text at the end of a file
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int i = 0, file;
