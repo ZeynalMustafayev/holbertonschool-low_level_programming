53a299b23e0246d3bad179b53f21953497f7d9bb
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-11 08:47:37 -0800

    Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_TRUNC | O_WRONLY | O_APPEND);
+	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (text_content == NULL)
 		text_content = "";
