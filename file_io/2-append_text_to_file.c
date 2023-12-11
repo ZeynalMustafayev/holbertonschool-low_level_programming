a0bec57fb4cf540b28d38fbcd9b0b72b3b9490cb
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-11 08:46:20 -0800

    Speak gently, she can hear

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i = 0, file;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_TRUNC | O_WRONLY | O_APPEND);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+	return (1);
+}
