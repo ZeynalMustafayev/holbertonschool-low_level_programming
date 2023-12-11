ec3705dd082ffd4b7fb7a8778d84f4ed2db00efa
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-11 08:20:16 -0800

    Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
+/**
+ * create_file - function that creates a file.
+ * @filename: variable pointer
+ * @text_content: content file
+ * Description: Create a function that creates a file.
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int i = 0, file;
