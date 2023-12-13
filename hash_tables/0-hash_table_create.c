ee06c592e059a15a987b17171a6254cbb6439530
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-13 08:02:25 -0800

    >>> ht = {}

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,6 +12,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ar;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
@@ -19,6 +20,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ar = malloc(sizeof(hash_table_t) * size);
 	if (ar == NULL)
 		return (NULL);
+	ar->size = size;
 	ar->array = malloc(sizeof(hash_node_t *) * size);
+	if (ar->array == NULL)
+		return (NULL);
+	for (i = 0; i < ar->size; i++)
+		ar->array[i] = NULL;
 	return (ar);
 }
