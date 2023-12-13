16e67ad8eea1649fa222894c43d7b7fda8f98f22
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-13 06:43:09 -0800

    >>> ht = {}

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,19 +2,21 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
-
+/**
+ * hash_table_create - creating hash table
+ *
+ * @size: size of hash table
+ *
+ * Return: ar
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ar;
-	unsigned int i;
 
 	ar = malloc(sizeof(hash_table_t) * size);
 	if (ar == NULL)
 		return (NULL);
 	ar->size = size;
-	ar->array = calloc(ar->size, sizeof(hash_node_t));
-
-	for (i = 0; i < ar->size; i++)
-		ar->array[i] = NULL;
+	ar->array = malloc(sizeof(hash_node_t));
 	return (ar);
 }
