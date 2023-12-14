4bc0ff83357f2aa1559c25d93855e14bc7878f91
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-14 05:47:38 -0800

    key -> index

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -2,10 +2,19 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
-
+/**
+ * key_index - find key index in hash
+ *
+ * @key: value key
+ *
+ * @size: hash size
+ *
+ * Return: index
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	int index;
+
 	index = hash_djb2(key) % size;
 	return (index);
 }
