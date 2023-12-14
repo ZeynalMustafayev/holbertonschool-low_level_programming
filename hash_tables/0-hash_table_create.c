95302226e0ffaab3f2c0cd2dd3b86d0eff9b5060
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-14 04:08:31 -0800

    >>> ht = {}

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	ar = malloc(sizeof(hash_table_t) * size);
+	ar = malloc(sizeof(hash_table_t *) * size);
 	if (ar == NULL)
 		return (NULL);
 	ar->size = size;
