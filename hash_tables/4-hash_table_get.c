0241be1a8e7948a3317433ed886d106513b61d19
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-16 03:14:12 -0800

    >>> ht['betty'] = 'cool'

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -22,8 +22,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (index >= ht->size)
 		return (NULL);
 	node = ht->array[index];
-	while (node && strcmp(node->key, key) != 0)
-		node = node->next;
-
+	while (node && node->key)
+	{
+		if (node && strcmp(node->key, key) != 0)
+			node = node->next;
+		else
+			break;
+	}
 	return ((node == NULL) ? NULL : node->value);
 }
