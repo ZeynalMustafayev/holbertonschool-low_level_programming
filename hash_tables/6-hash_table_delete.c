ef88caf2bf75a1c1d5d6d6b8c8b4e9cb274bf866
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-16 05:42:17 -0800

    >>> del ht

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp, *node;
+	unsigned long int i;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
