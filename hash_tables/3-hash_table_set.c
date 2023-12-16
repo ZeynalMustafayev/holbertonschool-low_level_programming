909f53d43bd17054e53b6f4bd3604eda387112fe
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-16 09:19:37 -0800

    6

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			if (!strcmp(current->key, key))
 			{
 				free(new_node);
-				free(current->key);
+				free(current->array[index]);
 				current->value = strdup(value);
 				return (1);
 			}
