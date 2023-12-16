ab03324cf2e5a3b9a99284edca11d745a6a8c156
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-16 05:02:45 -0800

    oldum

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -33,35 +33,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t *));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = strdup((char *)key);
-	new_node->value = strdup((char *)value);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
 	else
 	{
 		current = ht->array[index];
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next;
-			ht->array[index] = new_node;
-			free_node(current);
-			return (1);
-		}
-		while (current->next != NULL && strcmp(current->next->key, key) != 0)
-		{ current = current->next;
-		}
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next->next;
-			free_node(current->next);
-			current->next = new_node;
-		}
-		else
-		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
-		}
+		do {
+			if (!strcmp(current->key, key))
+			{
+				current->value = strdup(value);
+				return (1);
+			}
+			if (current->next)
+				current = current->next;
+		} while(current->next);
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
 	}
 	return (1);
 }
