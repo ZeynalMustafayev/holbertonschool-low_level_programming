f24713393f6c0c8b9f6edd1cb440e2781f668bac
Zeynal <zeynal.mustafayev.099@gmail.com>
2023-12-16 09:49:27 -0800

    6

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -42,7 +42,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				current = current->next;
 			}
 		} while (current->next);
-		free(current->key);
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
