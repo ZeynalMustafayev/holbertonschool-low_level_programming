bb855a5af95ef1088c03ecb029ca8e14e3d7709a
Zeynal <zeynal.mustafayev.099@gmail.com>
2024-01-16 02:42:39 -0800

    add printf 2 different place for print error message

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -16,8 +16,10 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
+			printf("Found at index: %lu\n", i);
 			return (i);
 		}
 	}
